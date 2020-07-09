66c5f20c55cf6a44c55eb326cb35f68ece4c153f
ICEworld27 <56315823+ICEworld27@users.noreply.github.com>
2020-07-09 18:52:46 +0500

    Add files via upload

---
+++ CalcCS-master/Expressions/ModuloOperation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace CalcCS.Expressions
+{
+    /// <summary>
+    /// Операция остаток от деления
+    /// </summary>
+    class ModuloOperation : BinaryOperation
+    {
+        /// <summary>
+        /// Конструктор
+        /// Обычно наследуется без изменений в классах конкретных операций.
+        /// </summary>
+        /// <param name="left">левый операнд</param>
+        /// <param name="right">правый операнд</param>
+        public ModuloOperation(IExpression left, IExpression right) : base(left, right)
+        { }
+
+        public override double Calculate()
+        {
+            return ((int)Math.Round(_left.Calculate())) % ((int)Math.Round(_right.Calculate()));
+        }
+    }
+}
---
+++ CalcCS-master/Expressions/PowerOperation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CalcCS.Expressions
+{
+    /// <summary>
+    /// Операция возведения в степень
+    /// </summary>
+    class PowerOperation : BinaryOperation
+    {
+        /// <summary>
+        /// Конструктор
+        /// Обычно наследуется без изменений в классах конкретных операций.
+        /// </summary>
+        /// <param name="left">левый операнд</param>
+        /// <param name="right">правый операнд</param>
+        public PowerOperation(IExpression left, IExpression right) : base(left, right)
+        { }
+        public override double Calculate()
+        {
+            return Math.Pow(_left.Calculate(), _right.Calculate());
+        }
+    }
+}
